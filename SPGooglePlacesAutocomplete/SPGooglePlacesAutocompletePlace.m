15073691bbfcacf4bb38da72259b5e0a5d98a313
Thomas Duplomb <thomas.duplomb@gmail.com>
2014-05-23 11:25:15 +0200

    Use coordinate we get back from google places details when possible

--- SPGooglePlacesAutocomplete/SPGooglePlacesAutocompletePlace.m
+++ SPGooglePlacesAutocomplete/SPGooglePlacesAutocompletePlace.m
@@ -41,61 +41,38 @@
     return geocoder;
 }
 
-- (void)resolveEstablishmentPlaceToPlacemark:(SPGooglePlacesPlacemarkResultBlock)block {
-    SPGooglePlacesPlaceDetailQuery *query = [[SPGooglePlacesPlaceDetailQuery alloc] initWithApiKey:self.key];
-    query.reference = self.reference;
-    [query fetchPlaceDetail:^(NSDictionary *placeDictionary, NSError *error) {
+- (void)resolveToPlacemarkFromLocation:(NSDictionary*)placeDictionary withBlock:(SPGooglePlacesPlacemarkResultBlock)block {
+    NSDictionary *locationDictionnary = [[placeDictionary objectForKey:@"geometry"] objectForKey:@"location"];
+    CLLocation *location;
+
+    if ([locationDictionnary objectForKey:@"lat"] && [locationDictionnary objectForKey:@"lng"]) {
+        CGFloat latitude = 360.0f;
+        CGFloat longitude = 360.0f;
+
+        if ([locationDictionnary objectForKey:@"lat"]) {
+            latitude = [[locationDictionnary objectForKey:@"lat"] floatValue];
+        }
+
+        if ([locationDictionnary objectForKey:@"lng"]) {
+            longitude = [[locationDictionnary objectForKey:@"lng"] floatValue];
+        }
+
+        location = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+    }
+
+    [[self geocoder] reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
         if (error) {
             block(nil, nil, error);
-        } else {
-            // Create palcemark from coordinates
-            if ([placeDictionary objectForKey:@"geometry"]) {
-                NSDictionary *locationDictionnary = [[placeDictionary objectForKey:@"geometry"] objectForKey:@"location"];
-                CLLocation *location;
-
-                if ([locationDictionnary objectForKey:@"lat"] && [locationDictionnary objectForKey:@"lng"]) {
-                    CGFloat latitude = 360.0f;
-                    CGFloat longitude = 360.0f;
-
-                    if ([locationDictionnary objectForKey:@"lat"]) {
-                        latitude = [[locationDictionnary objectForKey:@"lat"] floatValue];
-                    }
-
-                    if ([locationDictionnary objectForKey:@"lng"]) {
-                        longitude = [[locationDictionnary objectForKey:@"lng"] floatValue];
-                    }
-
-                    location = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
-                }
-
-                [[self geocoder] reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
-                    if (error) {
-                        block(nil, nil, error);
-                    }
-                    else {
-                        CLPlacemark *placemark = [placemarks onlyObject];
-                        block(placemark, self.name, error);
-                    }
-                }];
-            }
-            else {
-                // use geocoder
-                NSString *addressString = placeDictionary[@"formatted_address"];
-                [[self geocoder] geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
-                    if (error) {
-                        block(nil, nil, error);
-                    } else {
-                        CLPlacemark *placemark = [placemarks onlyObject];
-                        block(placemark, self.name, error);
-                    }
-                }];
-            }
+        }
+        else {
+            CLPlacemark *placemark = [placemarks onlyObject];
+            block(placemark, self.name, error);
         }
     }];
 }
 
-- (void)resolveGecodePlaceToPlacemark:(SPGooglePlacesPlacemarkResultBlock)block {
-    [[self geocoder] geocodeAddressString:self.name completionHandler:^(NSArray *placemarks, NSError *error) {
+- (void)resolveToPlacemarkFromAdress:(NSString*)addressString withBlock:(SPGooglePlacesPlacemarkResultBlock)block {
+    [[self geocoder] geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
         if (error) {
             block(nil, nil, error);
         } else {
@@ -106,13 +83,23 @@
 }
 
 - (void)resolveToPlacemark:(SPGooglePlacesPlacemarkResultBlock)block {
-    if (self.type == SPPlaceTypeGeocode) {
-        // Geocode places already have their address stored in the 'name' field.
-        [self resolveGecodePlaceToPlacemark:block];
-    } else {
-        [self resolveEstablishmentPlaceToPlacemark:block];
-    }
-}
+    SPGooglePlacesPlaceDetailQuery *query = [[SPGooglePlacesPlaceDetailQuery alloc] initWithApiKey:self.key];
 
+    query.reference = self.reference;
+    [query fetchPlaceDetail:^(NSDictionary *placeDictionary, NSError *error) {
+        if (error) {
+            block(nil, nil, error);
+        } else {
+            // Create palcemark from coordinates
+            if ([placeDictionary objectForKey:@"geometry"]) {
+                [self resolveToPlacemarkFromLocation:placeDictionary withBlock:block];
+            }
+            else {
+                // use geocoder
+                [self resolveToPlacemarkFromAdress:placeDictionary[@"formatted_address"] withBlock:block];
+            }
+        }
+    }];
+}
 
 @end
